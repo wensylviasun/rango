f2af6c09ebde21ac533005ccd353fec2cbc0e840
Wen Sun <wen@uber.com>
2018-05-23 10:10:09 -0700

    add link back to index page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<a href="about/">about</a>')
+    return HttpResponse('<a href="/rango/about/">About</a>')
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.")
+    return HttpResponse('Rango says here is the about page.<br/><a href="/rango/">Index</a>')
