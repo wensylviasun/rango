51876008192d12c4cc4dbed479656aa53cf43362
Wen Sun <wen@uber.com>
2018-05-23 10:07:32 -0700

    add about page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse('<a href="about/">about</a>')
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.")
